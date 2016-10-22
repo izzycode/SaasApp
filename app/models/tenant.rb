3f8ebe819142caaed555fabc7b303222ff1a7392
izzycode <marcemc@yahoo.com>
2016-10-21 22:26:53 -0400

    Scaffolded Projects

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -2,7 +2,8 @@ class Tenant < ActiveRecord::Base
 
    acts_as_universal_and_determines_tenant
   has_many :members, dependent: :destroy
-  validates_uniqueness_and_prescence_of :name
+  validates_uniqueness_of :name
+  validates_presence_of :name
 
     def self.create_new_tenant(tenant_params, user_params, coupon_params)
 
